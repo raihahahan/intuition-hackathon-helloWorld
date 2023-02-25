cd95f00d0a25bcfa7140d7bd22e76dc3673f10bd
Raihan Rizqullah <mraihandev@gmail.com>
2023-02-25 17:11:17 +0800

    add signalr client to webpage

---
+++ Client/helloworld-client/src/config.ts
@@ -0,0 +1,4 @@
+export default {
+  INPUT_RESULT_HUB: "https://localhost:50638/hubs/messages",
+  SIGNALR_CLIENT_METHOD: "sendinputresult",
+};
---
+++ Client/helloworld-client/src/server/socket.ts
@@ -0,0 +1,32 @@
+import * as signalR from "@microsoft/signalr";
+import CONFIG from "../config";
+
+class SocketConnection {
+  private connection;
+  private topics: string[] = [];
+  constructor() {
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl(CONFIG.INPUT_RESULT_HUB, {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets,
+      })
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+  }
+
+  async start(onReceiveMessage: (data: string) => void) {
+    try {
+      if (this.connection.state != signalR.HubConnectionState.Connected) {
+        await this.connection.start();
+        console.log("connection start");
+        this.connection.on(CONFIG.SIGNALR_CLIENT_METHOD, onReceiveMessage);
+      }
+    } catch (error) {
+      console.log("error loaded" + error);
+      setTimeout(this.start, 5000);
+    }
+  }
+}
+
+const messageConnection = new SocketConnection();
+export default messageConnection;
